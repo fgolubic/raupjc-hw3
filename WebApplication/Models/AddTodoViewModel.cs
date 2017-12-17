ef8d5061224f5b04bc56e8d59f63a609fe4cb563
Filip Golubić <filip.golubic@fer.hr>
2017-12-17 22:40:54 +0100

    Last Commit

---
+++ WebApplication/Models/AddTodoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class AddTodoViewModel
+    {
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime? DateDue { get; set; }
+
+        public string Label { get; set; }
+    }
+}
---
+++ WebApplication/Models/TodoViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoRepository;
+
+namespace WebApplication.Models
+{
+    public class TodoViewModel
+    {
+      
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public DateTime? DateCompleted { get; set; }
+        public DateTime? DateDue { get; set; }
+        public string Labels { get; set; }
+
+
+        public TodoViewModel(TodoItem item)
+        {
+            Id = item.Id;
+            Text = item.Text;
+            DateCompleted = item.DateCompleted;
+            DateDue = item.DateDue;
+        }
+
+        public TodoViewModel()
+        {
+        }
+
+        public string DaysRemaining()
+        {
+            if (DateDue == null)
+            {
+                return " ";
+            }
+
+            else
+            {
+                return "za " + ((DateTime) DateDue - DateTime.Now).Days.ToString() + " dana!";
+            }
+        }
+    }
+}
